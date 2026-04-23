b8f3d5956ff6ca1e5752578feee357e23a040e36
nee-tin <nkrkey@gmail.com>
2026-04-23 23:44:15 +0545

    Update Home.jsx1012

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Footer from "../components/Footer"
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col">
       
       <main className="flex-1">
         <Hero />
