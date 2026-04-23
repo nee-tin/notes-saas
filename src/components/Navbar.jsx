9a2a66014fa2ce8425e120080bc32d0d9ca996e6
nee-tin <nkrkey@gmail.com>
2026-04-23 23:50:20 +0545

    Update Navbar.jsx.btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,10 +57,10 @@ function Navbar() {
             </button>
           </div>
         ) : (
-           <button
+          <button
   onClick={handleDemoLogin}
   className="
-    bg-indigo-700 text-white px-4 py-2 mr-8 rounded-lg
+    bg-indigo-700 text-white px-4 py-2 ml-auto rounded-lg
     shadow-sm hover:shadow-md
     transition-all duration-200 ease-in-out
     hover:-translate-y-[1px]
